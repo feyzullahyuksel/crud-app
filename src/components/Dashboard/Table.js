e217bacf9f2732703f5fe843d6f8df9c44a43b30
Feyzullah Yüksel <info@feyzullahyuksel.com>
2022-05-16 19:19:37 +0200

    add striped-table-blue class

--- src/components/Dashboard/Table.js
+++ src/components/Dashboard/Table.js
@@ -13,7 +13,7 @@ const Table = ({ employees, handleEdit, handleDelete }) => {
 
   return (
     <div className="contain-table">
-      <table className="striped-table">
+      <table className="striped-table striped-table-blue">
         <thead>
           <tr>
             <th>No.</th>
